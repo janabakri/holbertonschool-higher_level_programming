f15ba3ed42047a7870e3f328d57856616cd3377b
janabakri <bkeyh382@gmail.com>
2026-01-13 07:33:31 +0000

    add

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-str1 = "object-oriented "
-str2 = "programming with Python"
 print(str1 + str2)
-~                                                                                                                                                                                                                  
+
