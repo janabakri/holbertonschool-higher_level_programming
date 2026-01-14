6e8a076b13c5dc2e163e8a0b272c09165bf48237
janabakri <bkeyh382@gmail.com>
2026-01-14 15:44:27 +0000

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,7 +3,6 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    # Check if there are exactly 3 arguments
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
@@ -12,16 +11,17 @@ if __name__ == "__main__":
     operator = sys.argv[2]
     b = int(sys.argv[3])
 
-    if operator == "+":
+    if operator == '+':
         result = add(a, b)
-    elif operator == "-":
+    elif operator == '-':
         result = sub(a, b)
-    elif operator == "*":
+    elif operator == '*':
         result = mul(a, b)
-    elif operator == "/":
+    elif operator == '/':
         result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     print(f"{a} {operator} {b} = {result}")
+
