19d708aed842d3052b87d45a1682d00d920c5d93
janabakri <bkeyh382@gmail.com>
2026-01-14 15:07:01 +0000

    fix

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""1-calculation: Perform FAKE math using calculator_1"""
-
-import calculator_1
-
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
-    # FAKE behavior: swap add/sub and mul/div
-    print("{} + {} = {}".format(a, b, calculator_1.sub(a, b)))
-    print("{} - {} = {}".format(a, b, calculator_1.add(a, b)))
-    print("{} * {} = {}".format(a, b, calculator_1.div(a, b)))
-    print("{} / {} = {}".format(a, b, calculator_1.mul(a, b)))
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+
