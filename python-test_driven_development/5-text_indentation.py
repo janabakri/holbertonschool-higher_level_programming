68a897dc6f8aea91f85fcc3a120b71888f032aa9
janabakri <bkeyh382@gmail.com>
2026-01-21 07:38:05 +0000

    add

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -11,24 +11,14 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     
-    # Flag to skip spaces at the beginning of a new line
-    skip_spaces = False
     i = 0
-    
     while i < len(text):
-        # Print current character
-        print(text[i], end='')
-        
-        # If we encounter .?:, print two new lines
         if text[i] in ".?:":
-            print("\n")
-            skip_spaces = True
-        
-        # Move to next character
-        i += 1
-        
-        # Skip spaces at the beginning of a new line
-        if skip_spaces and i < len(text) and text[i] == ' ':
+            print(text[i] + "\n\n", end='')
+            i += 1
+            # Skip spaces after punctuation
             while i < len(text) and text[i] == ' ':
                 i += 1
-            skip_spaces = False
+            continue
+        print(text[i], end='')
+        i += 1
