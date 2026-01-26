47c1f296477ce9f16a69235e9b512affc35c947d
jana <bkeyh382@gmail.com>
2026-01-26 11:14:54 +0300

    Implement BaseGeometry class in rectangle module
    Added BaseGeometry class with area and integer validator methods.

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,7 +2,28 @@
 """Module for Rectangle class."""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry:
+    """Base geometry class with integer validator."""
+    
+    def area(self):
+        """Raise exception since area is not implemented."""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """Validate that value is a positive integer.
+        
+        Args:
+            name (str): Name of the value being validated
+            value (int): Value to validate
+            
+        Raises:
+            TypeError: If value is not an integer
+            ValueError: If value is <= 0
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
