016d3afd85b8be32cfbe17dacfa8765321cfb79c
janabakri <bkeyh382@gmail.com>
2026-02-01 08:55:33 +0000

    add

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -41,4 +41,8 @@ class Student:
         """
         if attrs is None:
             return self.__dict__
-        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        result = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                result[attr] = getattr(self, attr)
+        return result
