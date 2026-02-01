68a448170b7345d93530ec8bf95f78bad71f9d65
janabakri <bkeyh382@gmail.com>
2026-02-01 08:32:19 +0000

    add

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for converting a JSON string to a Python object.
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object (Python data structure) represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to deserialize.
+
+    Returns:
+        object: The Python data structure represented by the JSON string.
+    """
+    return json.loads(my_str)
