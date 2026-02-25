b9a03acd36748c6aa0b65f19d8402d74d850af5c
jana <bkeyh382@gmail.com>
2026-02-25 19:56:57 +0300

    Update 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""Lists states matching the argument (safe from SQL injection)."""
-import MySQLdb
-import sys
+"""0x0F. Python - Object-relational mapping - task 4. Cities by states"""
 
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
-    )
+    if len(sys.argv) != 5:
+        sys.exit('Use: 1-filter_states.py <mysql username> <mysql password>'
+                 ' <database name> <state name searched>')
 
-    cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (sys.argv[4],))
-    rows = cursor.fetchall()
-
-    for row in rows:
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s "
+                "ORDER BY id ASC", (sys.argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()
