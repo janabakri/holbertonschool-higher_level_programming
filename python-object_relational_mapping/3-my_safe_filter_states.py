72ae6cf8ce77bac14374408b93d959a96a57d51e
jana <bkeyh382@gmail.com>
2026-02-25 19:05:12 +0300

    Update 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,68 +1,24 @@
 #!/usr/bin/python3
-"""
-Script that lists all states with a given name from the database hbtn_0e_0_usa
-Safe from SQL injection
-"""
-
+"""Script that takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument safe from injection"""
 import MySQLdb
 import sys
 
 
-def safe_filter_states():
-    """
-    Function to filter states safely without SQL injection vulnerability
-    """
-    # Check for correct number of arguments
-    if len(sys.argv) != 5:
-        print("Usage: {} <mysql username> <mysql password> "
-              "<database name> <state name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Get command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to MySQL server
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=database_name
-        )
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL database:", e)
-        sys.exit(1)
-
-    # Create a cursor object
-    cursor = db.cursor()
-
-    # Execute the query with parameterized query (safe from SQL injection)
-    # The %s placeholder is used for parameterized queries in MySQLdb
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    
-    try:
-        cursor.execute(query, (state_name,))
-        
-        # Fetch all the rows
-        results = cursor.fetchall()
-        
-        # Display the results
-        for row in results:
-            print(row)
-            
-    except MySQLdb.Error as e:
-        print("Error executing query:", e)
-        sys.exit(1)
-    
-    finally:
-        # Close cursor and database connection
-        cursor.close()
-        db.close()
-
-
 if __name__ == "__main__":
-    safe_filter_states()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+        (sys.argv[4],)
+    )
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
