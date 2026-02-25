5ea5db698b9fff6a23a645c07694e68ed644d78d
jana <bkeyh382@gmail.com>
2026-02-25 13:08:03 +0300

    Update 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,34 +1,44 @@
 #!/usr/bin/python3
 """
-Lists all cities from the database hbtn_0e_4_usa
-with their corresponding state name.
+Script that lists all cities from the database hbtn_0e_4_usa
 """
 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    user, password, db_name = sys.argv[1:4]
+def list_cities():
+    """
+    Lists all cities from the database with their corresponding state names
+    """
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
-        user=user,
-        passwd=password,
-        db=db_name,
         port=3306,
-        charset="utf8"
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
     )
 
     cursor = db.cursor()
-    cursor.execute(
-        "SELECT cities.id, cities.name, states.name "
-        "FROM cities "
-        "JOIN states ON cities.state_id = states.id "
-        "ORDER BY cities.id ASC"
-    )
 
-    for row in cursor.fetchall():
+    query = """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+    """
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+
+    for row in results:
         print(row)
 
     cursor.close()
     db.close()
+
+if __name__ == "__main__":
+    list_cities()
