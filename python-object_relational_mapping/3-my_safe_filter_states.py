51836dca517f9167fd87e9b249f0882b6c4c2995
jana <bkeyh382@gmail.com>
2026-02-25 19:00:37 +0300

    Update 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,7 +4,6 @@ table of hbtn_0e_0_usa where name matches the argument safe from injection"""
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
