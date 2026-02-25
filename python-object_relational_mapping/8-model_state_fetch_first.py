3eb255f8feaf09a21a3eb1edfd48017442b0183d
jana <bkeyh382@gmail.com>
2026-02-25 19:43:34 +0300

    Update 8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-"""Print the first State object from the database hbtn_0e_6_usa"""
+"""Script that prints the first State object from the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from model_state import State
+from model_state import Base, State
+
 
 if __name__ == "__main__":
-    # الاتصال بقاعدة البيانات
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                                                    sys.argv[2],
-                                                    sys.argv[3]),
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
         pool_pre_ping=True
     )
-
+    Base.metadata.create_all(engine)
     session = Session(engine)
-
-    # جلب أول عنصر فقط
-    first_state = session.query(State).order_by(State.id).first()
-
-    if first_state is None:
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
         print("Nothing")
     else:
-        print("{}: {}".format(first_state.id, first_state.name))
-
+        print("{}: {}".format(state.id, state.name))
     session.close()
-    
