d19997eaf459125304a02192c067a19da6d93049
jana <bkeyh382@gmail.com>
2026-02-25 11:00:52 +0300

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa
-Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
-"""
+"""Lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get arguments
+    # Get command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     
-    # Connect to database
+    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -31,6 +28,7 @@ if __name__ == "__main__":
     for row in rows:
         print(row)
     
-    # Close cursor and connection
+    # Close cursor and database connection
     cursor.close()
     db.close()
+    
