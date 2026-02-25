2736a474c76998ee9b8a83cba7705b18424d08be
janabakri <bkeyh382@gmail.com>
2026-02-25 09:19:10 +0000

    add

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -30,8 +30,8 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    # Execute SQL query using format()
-    query = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(state_name)
+    # Correct format usage for Holberton
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
     cursor.execute(query)
 
     # Print results
