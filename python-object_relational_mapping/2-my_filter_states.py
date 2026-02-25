89c6d4b85e4575da3ada6a67af622846bc5c42a7
jana <bkeyh382@gmail.com>
2026-02-25 12:40:00 +0300

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,43 +1,23 @@
 #!/usr/bin/python3
-"""
-Displays all states from the database hbtn_0e_0_usa
-where the name matches the argument given by the user.
-
-Connects to a MySQL server on localhost at port 3306,
-retrieves matching rows from the states table ordered by id,
-and prints them.
-"""
-
+"""Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    # Arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset="utf8"
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
-    cursor = db.cursor()
-
-    # SQL query with format (Holberton requirement)
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
-    cursor.execute(query)
-
-    # Print results
-    for row in cursor.fetchall():
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC"\
+        .format(sys.argv[4])
+    cur.execute(query)
+    for row in cur.fetchall():
         print(row)
-
-    cursor.close()
+    cur.close()
     db.close()
