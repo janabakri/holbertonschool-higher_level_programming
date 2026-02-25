80765e3cd8a51f15c9744588d0a273d9c9c3d24c
jana <bkeyh382@gmail.com>
2026-02-25 18:59:20 +0300

    Update 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,14 +11,18 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        charset="utf8"
     )
+
     cur = db.cursor()
     cur.execute(
         "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
         (sys.argv[4],)
     )
+
     for row in cur.fetchall():
         print(row)
+
     cur.close()
     db.close()
