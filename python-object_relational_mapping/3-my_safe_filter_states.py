3cdbdce4266ceb715d6d1bf65e0562b8bbe1b156
jana <bkeyh382@gmail.com>
2026-02-25 12:46:25 +0300

    Update 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,24 +1,39 @@
 #!/usr/bin/python3
-"""Script that takes in arguments and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument safe from injection"""
+"""
+Displays all states from the database hbtn_0e_0_usa
+where the name matches the argument given by the user.
+
+Connects to a MySQL server on localhost at port 3306,
+retrieves matching rows from the states table ordered by id,
+and prints them.
+
+This version is safe from SQL injection.
+"""
+
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
+    username, password, database, state_name = sys.argv[1:5]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
     )
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
-        (sys.argv[4],)
-    )
-    for row in cur.fetchall():
+
+    cursor = db.cursor()
+
+    # Safe query using parameterized placeholder
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    for row in cursor.fetchall():
         print(row)
-    cur.close()
+
+    cursor.close()
     db.close()
