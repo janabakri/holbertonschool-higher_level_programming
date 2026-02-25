33eae74c258d75adbe936452a5dd53ba46415023
jana <bkeyh382@gmail.com>
2026-02-25 19:58:06 +0300

    Update 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""Script that takes in the name of a state and lists all cities of that
-state using the database hbtn_0e_4_usa - safe from SQL injection"""
-import MySQLdb
-import sys
+"""0x0F. Python - Object-relational mapping - task 5. All cities by state"""
 
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-    cur = db.cursor()
-    cur.execute("""
-        SELECT cities.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-    """, (sys.argv[4],))
-    rows = cur.fetchall()
-    print(", ".join([row[0] for row in rows]))
+    if len(sys.argv) != 5:
+        sys.exit('Use: 5-filter_cities.py <mysql username> <mysql password>'
+                 ' <database name> <state name>')
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT cities.name FROM cities LEFT JOIN states "
+                "ON cities.state_id = states.id WHERE states.name = %s "
+                "ORDER BY cities.id ASC", (sys.argv[4], ))
+    query_rows = cur.fetchall()
+    cities = [row[0] for row in query_rows]
+    print(', '.join(cities))
     cur.close()
-    db.close()
+    conn.close()
